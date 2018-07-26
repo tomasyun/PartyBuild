9614e6ab4c15a6087cd3e916cbdffbdca54d08f6
yunt <yuntuo19900228@126.com>
2018-07-26 08:43:31 +0800

    ###新增ui界面/优化代码###

--- app/src/main/java/www/dico/cn/partybuild/UpdateManager.java
+++ app/src/main/java/www/dico/cn/partybuild/UpdateManager.java
@@ -27,7 +27,6 @@ public class UpdateManager {
         int nVersion_code = 1;
         if (mVersion_code < nVersion_code) {
             // 显示提示对话
-
             WindowManager.LayoutParams lp = mContext.getWindow().getAttributes();
             lp.alpha = 0.5f;
             mContext.getWindow().setAttributes(lp);
