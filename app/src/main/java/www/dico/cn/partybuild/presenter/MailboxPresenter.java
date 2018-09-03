3084f54c61edf71c08379917e91da41a8f39f48e
yunt <yuntuo19900228@126.com>
2018-09-03 17:49:00 +0800

    ####

--- app/src/main/java/www/dico/cn/partybuild/presenter/MailboxPresenter.java
+++ app/src/main/java/www/dico/cn/partybuild/presenter/MailboxPresenter.java
@@ -19,7 +19,7 @@ public class MailboxPresenter extends BaseMvpPresenter<MailboxView> {
         IProgressDialog dialog = new IProgressDialog() {
             @Override
             public Dialog getDialog() {
-                LoadingDialog.Builder builder = new LoadingDialog.Builder(AppManager.getManager().findActivity(MailboxActivity.class))
+                LoadingDialog.Builder builder = new LoadingDialog.Builder(AppManager.getManager().curActivity())
                         .setCancelable(true)
                         .setCancelOutside(true)
                         .setMessage("获取中..")
